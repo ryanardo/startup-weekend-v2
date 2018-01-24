5f468e90a5654aa13aee9cf090eac19ab17e6f87
Ryan G Ferris <ryanglenferris@gmail.com>
2018-01-24 08:14:24 -0800

    Attendee Testing Setup
    Attendee testing file setup.

--- src/test/java/dao/Attendee_DAO_SQL2OTest.java
+++ src/test/java/dao/Attendee_DAO_SQL2OTest.java
@@ -3,17 +3,27 @@ package dao;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-/**
- * Created by Guest on 1/21/18.
- */
 public class Attendee_DAO_SQL2OTest {
+
+    private Event_DAO_SQL2O event_DAO;
+    private Attendee_DAO_SQL2O attendee_DAO;
+    private Connection conn;
+
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        event_DAO = new Event_DAO_SQL2O(sql2o);
+        attendee_DAO = new Attendee_DAO_SQL2O(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
 
     @Test
